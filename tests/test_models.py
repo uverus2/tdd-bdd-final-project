e1b5ede7a22a53fa1e7faad55bec5f38fce6f77a
Konstantin Ruzhev <knruzhev@gmail.com>
2024-12-31 07:39:00 -0500

    Test

--- tests/test_models.py
+++ tests/test_models.py
@@ -101,6 +101,49 @@ class TestProductModel(unittest.TestCase):
         self.assertEqual(new_product.available, product.available)
         self.assertEqual(new_product.category, product.category)
 
-    #
-    # ADD YOUR TEST CASES HERE
-    #
+    def test_read_a_product(self):
+        """It should find the product in the DB"""
+        product = ProductFactory()
+        product.id = None
+        product.create()
+
+        self.assertIsNotNone(product.id)
+        
+        found_product = product.find(product.id)
+        self.assertEqual(found_product.id, product.id)
+        self.assertEqual(found_product.name, product.name)
+        self.assertEqual(found_product.description, product.description)
+        self.assertEqual(found_product.price, product.price)
+        self.assertEqual(found_product.available, product.available)
+        self.assertEqual(found_product.category, product.category)
+
+    def test_update_a_product(self):
+        """It should Update a Product"""
+        product = ProductFactory()
+        product.id = None
+        product.create()
+
+        self.assertIsNotNone(product.id)
+
+        product.description = "Updated"
+        product.name = "Hat"
+        product.price = 10.00
+
+        original_id = product.id
+        product.update()
+
+        self.assertEqual(product.id, original_id)
+        self.assertEqual(product.description, "Updated")
+        self.assertEqual(product.name, "Hat")
+        self.assertEqual(product.price, 10.00)
+
+        products = Product.all()
+        self.assertEqual(len(products), 1)
+        self.assertEqual(products[0].id, original_id)
+        self.assertEqual(products[0].name, "Hat")
+        self.assertEqual(products[0].description, "Updated")
+        self.assertEqual(products[0].price, 10.00)
+
+
+
+
